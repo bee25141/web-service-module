6bc16bf9529078efe7a0a3f80bd76d1d1a9bf3bd
bee25141 <bee25141@gmail.com>
2020-02-02 13:40:05 -0600

    created initial web module

---
+++ interface.tf
@@ -0,0 +1,58 @@
+variable "region" {}
+
+variable "ami" {
+
+    default = {
+        "us-east-1" = "ami-f652979b"
+        "us-west-1" = "ami-7c4b331c"
+    }
+  
+}
+
+variable "instance_type" {
+
+    default = "t2.micro"
+  
+}
+
+variable "key_name" {
+
+    default = "tfb"
+  
+}
+
+variable "environment" {}
+
+variable "vpc_id" {
+  
+}
+
+variable "public_subnet_ids" {
+
+    type = list(string)
+  
+}
+
+variable "domain" {
+  
+}
+
+variable "web_instance_count" {
+  
+}
+
+variable "app_instance_count" {
+  
+}
+
+output "web_elb_address" {
+  value = aws_elb.web.dns_name
+}
+
+output "web_host_addresses" {
+  value = aws_instance.web[*].private_ip
+}
+
+output "app_host_addresses" {
+  value = aws_instance.app[*].private_ip
+}
